9ed5f7df5411fe5a56277aa558ab60eb5a57a249
damienbod <damien_bod@hotmail.com>
2025-05-01 22:37:53 +0200

    UI

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -23,15 +23,15 @@ public static class Config
             // interactive client using code flow + pkce
             new Client
             {
-                ClientId = "interactive",
+                ClientId = "my-client",
                 ClientSecrets = { new Secret("44C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
                 RequirePushedAuthorization = true,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:5015/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:5015/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:5015/signout-callback-oidc" },
 
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile", "scope1" }

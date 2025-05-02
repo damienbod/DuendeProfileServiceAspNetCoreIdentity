395c2ec799b7a8a7055cf08bcfb853dd82296a93
damienbod <damien_bod@hotmail.com>
2025-05-02 21:31:02 +0200

    Update laims

--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -46,72 +46,73 @@ internal static class HostingExtensions
             options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
             options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
             options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-        }).AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, "Auth0", options =>
-            {
-                options.SignInScheme = "externalscheme";
-                options.SignOutScheme = IdentityConstants.ApplicationScheme;
-                options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
-                options.ClientId = builder.Configuration["Auth0:ClientId"];
-                options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
-                options.ResponseType = OpenIdConnectResponseType.Code;
-                options.Scope.Clear();
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("email");
-                options.Scope.Add("auth0-user-api-one");
-                // options.CallbackPath = new PathString("/signin-oidc");
-                options.ClaimsIssuer = "Auth0";
-                options.SaveTokens = true;
-                options.UsePkce = true;
-                options.GetClaimsFromUserInfoEndpoint = true;
-                options.TokenValidationParameters.NameClaimType = "name";
-                options.Events = new OpenIdConnectEvents
-                {
-                    OnTokenResponseReceived = context =>
-                    {
-                        var idToken = context.TokenEndpointResponse.IdToken;
-                        return Task.CompletedTask;
-                    }
-                };
+        })
+            //.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, "Auth0", options =>
+            //{
+            //    options.SignInScheme = "externalscheme";
+            //    options.SignOutScheme = IdentityConstants.ApplicationScheme;
+            //    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+            //    options.ClientId = builder.Configuration["Auth0:ClientId"];
+            //    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+            //    options.ResponseType = OpenIdConnectResponseType.Code;
+            //    options.Scope.Clear();
+            //    options.Scope.Add("openid");
+            //    options.Scope.Add("profile");
+            //    options.Scope.Add("email");
+            //    options.Scope.Add("auth0-user-api-one");
+            //    // options.CallbackPath = new PathString("/signin-oidc");
+            //    options.ClaimsIssuer = "Auth0";
+            //    options.SaveTokens = true;
+            //    options.UsePkce = true;
+            //    options.GetClaimsFromUserInfoEndpoint = true;
+            //    options.TokenValidationParameters.NameClaimType = "name";
+            //    options.Events = new OpenIdConnectEvents
+            //    {
+            //        OnTokenResponseReceived = context =>
+            //        {
+            //            var idToken = context.TokenEndpointResponse.IdToken;
+            //            return Task.CompletedTask;
+            //        }
+            //    };
 
-                options.Events = new OpenIdConnectEvents
-                {
-                    // handle the logout redirection 
-                    OnRedirectToIdentityProviderForSignOut = (context) =>
-                    {
-                        var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+            //    options.Events = new OpenIdConnectEvents
+            //    {
+            //        // handle the logout redirection 
+            //        OnRedirectToIdentityProviderForSignOut = (context) =>
+            //        {
+            //            var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
 
-                        var postLogoutUri = context.Properties.RedirectUri;
-                        if (!string.IsNullOrEmpty(postLogoutUri))
-                        {
-                            if (postLogoutUri.StartsWith("/"))
-                            {
-                                // transform to absolute
-                                var request = context.Request;
-                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                            }
-                            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-                        }
+            //            var postLogoutUri = context.Properties.RedirectUri;
+            //            if (!string.IsNullOrEmpty(postLogoutUri))
+            //            {
+            //                if (postLogoutUri.StartsWith("/"))
+            //                {
+            //                    // transform to absolute
+            //                    var request = context.Request;
+            //                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+            //                }
+            //                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+            //            }
 
-                        context.Response.Redirect(logoutUri);
-                        context.HandleResponse();
+            //            context.Response.Redirect(logoutUri);
+            //            context.HandleResponse();
 
-                        return Task.CompletedTask;
-                    },
-                    OnRedirectToIdentityProvider = context =>
-                    {
-                        // The context's ProtocolMessage can be used to pass along additional query parameters
-                        // to Auth0's /authorize endpoint.
-                        // 
-                        // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
-                        // to call protected endpoints on the corresponding API.
-                        context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
-                        context.ProtocolMessage.AcrValues = "http://schemas.openid.net/pape/policies/2007/06/multi-factor";
+            //            return Task.CompletedTask;
+            //        },
+            //        OnRedirectToIdentityProvider = context =>
+            //        {
+            //            // The context's ProtocolMessage can be used to pass along additional query parameters
+            //            // to Auth0's /authorize endpoint.
+            //            // 
+            //            // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
+            //            // to call protected endpoints on the corresponding API.
+            //            context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
+            //            context.ProtocolMessage.AcrValues = "http://schemas.openid.net/pape/policies/2007/06/multi-factor";
 
-                        return Task.FromResult(0);
-                    }
-                };
-            })
+            //            return Task.FromResult(0);
+            //        }
+            //    };
+            //})
             .AddMicrosoftIdentityWebApp(options =>
             {
                 builder.Configuration.Bind("AzureAd", options);
@@ -122,6 +123,11 @@ internal static class HostingExtensions
                 options.UsePkce = true;
                 options.Events = new OpenIdConnectEvents
                 {
+                    OnRedirectToIdentityProvider = context =>
+                    {
+                        context.ProtocolMessage.AcrValues = "http://schemas.openid.net/pape/policies/2007/06/multi-factor";
+                        return Task.FromResult(0);
+                    },
                     OnTokenResponseReceived = context =>
                     {
                         var idToken = context.TokenEndpointResponse.IdToken;

4a2867ecf1aae6339a4c251f269c945437571d34
damienbod <damien_bod@hotmail.com>
2025-05-23 08:40:24 +0200

    clean up

--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -46,98 +46,98 @@ internal static class HostingExtensions
             options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
             options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
         })
-            .AddOpenIdConnect("Auth0Scheme", "Auth0", options =>
-            {
-                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                options.SignOutScheme = IdentityConstants.ApplicationScheme;
-                options.CallbackPath = new PathString("/signin-oidc-auth0");
-                options.RemoteSignOutPath = new PathString("/signout-callback-oidc-auth0");
-                options.SignedOutCallbackPath = new PathString("/signout-oidc-auth0");
+        .AddOpenIdConnect("Auth0Scheme", "Auth0", options =>
+        {
+            options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            options.SignOutScheme = IdentityConstants.ApplicationScheme;
+            options.CallbackPath = new PathString("/signin-oidc-auth0");
+            options.RemoteSignOutPath = new PathString("/signout-callback-oidc-auth0");
+            options.SignedOutCallbackPath = new PathString("/signout-oidc-auth0");
 
-                options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
-                options.ClientId = builder.Configuration["Auth0:ClientId"];
-                options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
-                options.ResponseType = OpenIdConnectResponseType.Code;
-                options.Scope.Clear();
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("email");
-                options.Scope.Add("auth0-user-api-one");
+            options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+            options.ClientId = builder.Configuration["Auth0:ClientId"];
+            options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+            options.ResponseType = OpenIdConnectResponseType.Code;
+            options.Scope.Clear();
+            options.Scope.Add("openid");
+            options.Scope.Add("profile");
+            options.Scope.Add("email");
+            options.Scope.Add("auth0-user-api-one");
                 
-                options.ClaimsIssuer = "Auth0";
-                options.SaveTokens = true;
-                options.UsePkce = true;
-                options.GetClaimsFromUserInfoEndpoint = true;
-                options.TokenValidationParameters.NameClaimType = "name";
-                options.Events = new OpenIdConnectEvents
-                {
-                    OnTokenResponseReceived = context =>
-                    {
-                        var idToken = context.TokenEndpointResponse.IdToken;
-                        return Task.CompletedTask;
-                    }
-                };
-
-                options.Events = new OpenIdConnectEvents
-                {
-                    // handle the logout redirection 
-                    OnRedirectToIdentityProviderForSignOut = (context) =>
-                    {
-                        var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
-
-                        var postLogoutUri = context.Properties.RedirectUri;
-                        if (!string.IsNullOrEmpty(postLogoutUri))
-                        {
-                            if (postLogoutUri.StartsWith("/"))
-                            {
-                                // transform to absolute
-                                var request = context.Request;
-                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                            }
-                            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-                        }
-
-                        context.Response.Redirect(logoutUri);
-                        context.HandleResponse();
-
-                        return Task.CompletedTask;
-                    },
-                    OnRedirectToIdentityProvider = context =>
-                    {
-                        // The context's ProtocolMessage can be used to pass along additional query parameters
-                        // to Auth0's /authorize endpoint.
-                        // 
-                        // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
-                        // to call protected endpoints on the corresponding API.
-                        context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
-                        context.ProtocolMessage.AcrValues = "http://schemas.openid.net/pape/policies/2007/06/multi-factor";
-
-                        return Task.FromResult(0);
-                    }
-                };
-            })
-            .AddOpenIdConnect("EntraID", "EntraID", oidcOptions =>
+            options.ClaimsIssuer = "Auth0";
+            options.SaveTokens = true;
+            options.UsePkce = true;
+            options.GetClaimsFromUserInfoEndpoint = true;
+            options.TokenValidationParameters.NameClaimType = "name";
+            options.Events = new OpenIdConnectEvents
             {
-                builder.Configuration.Bind("AzureAd", oidcOptions);
-                oidcOptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                oidcOptions.SignOutScheme = IdentityConstants.ApplicationScheme;
+                OnTokenResponseReceived = context =>
+                {
+                    var idToken = context.TokenEndpointResponse.IdToken;
+                    return Task.CompletedTask;
+                }
+            };
 
-                oidcOptions.CallbackPath = new PathString("/signin-oidc");
-                oidcOptions.RemoteSignOutPath = new PathString("/signout-callback-oidc");
-                oidcOptions.SignedOutCallbackPath = new PathString("/signout-oidc");
+            options.Events = new OpenIdConnectEvents
+            {
+                // handle the logout redirection 
+                OnRedirectToIdentityProviderForSignOut = (context) =>
+                {
+                    var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
 
-                oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
-                oidcOptions.Scope.Add("user.read");
-                oidcOptions.Scope.Add(OpenIdConnectScope.OfflineAccess);
-                oidcOptions.Authority = $"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/v2.0/";
-                oidcOptions.ClientId = builder.Configuration["AzureAd:ClientId"];
-                oidcOptions.ClientSecret = builder.Configuration["AzureAd:ClientSecret"];
-                oidcOptions.ResponseType = OpenIdConnectResponseType.Code;
-                oidcOptions.MapInboundClaims = false;
-                oidcOptions.SaveTokens = true;
-                oidcOptions.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
-                oidcOptions.TokenValidationParameters.RoleClaimType = "role";
-            });
+                    var postLogoutUri = context.Properties.RedirectUri;
+                    if (!string.IsNullOrEmpty(postLogoutUri))
+                    {
+                        if (postLogoutUri.StartsWith("/"))
+                        {
+                            // transform to absolute
+                            var request = context.Request;
+                            postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                        }
+                        logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                    }
+
+                    context.Response.Redirect(logoutUri);
+                    context.HandleResponse();
+
+                    return Task.CompletedTask;
+                },
+                OnRedirectToIdentityProvider = context =>
+                {
+                    // The context's ProtocolMessage can be used to pass along additional query parameters
+                    // to Auth0's /authorize endpoint.
+                    // 
+                    // Set the audience query parameter to the API identifier to ensure the returned Access Tokens can be used
+                    // to call protected endpoints on the corresponding API.
+                    context.ProtocolMessage.SetParameter("audience", "https://auth0-api1");
+                    context.ProtocolMessage.AcrValues = "http://schemas.openid.net/pape/policies/2007/06/multi-factor";
+
+                    return Task.FromResult(0);
+                }
+            };
+        })
+        .AddOpenIdConnect("EntraID", "EntraID", oidcOptions =>
+        {
+            builder.Configuration.Bind("AzureAd", oidcOptions);
+            oidcOptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            oidcOptions.SignOutScheme = IdentityConstants.ApplicationScheme;
+
+            oidcOptions.CallbackPath = new PathString("/signin-oidc");
+            oidcOptions.RemoteSignOutPath = new PathString("/signout-callback-oidc");
+            oidcOptions.SignedOutCallbackPath = new PathString("/signout-oidc");
+
+            oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+            oidcOptions.Scope.Add("user.read");
+            oidcOptions.Scope.Add(OpenIdConnectScope.OfflineAccess);
+            oidcOptions.Authority = $"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/v2.0/";
+            oidcOptions.ClientId = builder.Configuration["AzureAd:ClientId"];
+            oidcOptions.ClientSecret = builder.Configuration["AzureAd:ClientSecret"];
+            oidcOptions.ResponseType = OpenIdConnectResponseType.Code;
+            oidcOptions.MapInboundClaims = false;
+            oidcOptions.SaveTokens = true;
+            oidcOptions.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
+            oidcOptions.TokenValidationParameters.RoleClaimType = "role";
+        });
         // This code creates it own scheme and so does not fit well into identity and external authentication
         //.AddMicrosoftIdentityWebApp(options =>
         //{
